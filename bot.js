de03675888200310ff1d95c909756670eb5f17b6
DaSupaFl00F <49820230+DaSupaFl00F@users.noreply.github.com>
2019-05-04 14:07:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Ready!');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '.ping') {
-    	message.reply('haha yes');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
