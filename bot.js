704f0eafb3569872cfb991f6ec22cf4bd2ea42ca
DaSupaFl00F <49820230+DaSupaFl00F@users.noreply.github.com>
2019-05-04 14:21:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '.ping') {
+    	message.reply('haha yes');
   	}
 });
 
